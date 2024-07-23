46ea5ef0c03b5853ca0a998b16b02055ec68e33e
박종국 <whdrnrdl789@naver.com>
2024-07-23 23:18:39 +0900

    도메인 분리

--- src/main/java/com/example/tech_blog/service/UpdateService.java
+++ src/main/java/com/example/tech_blog/service/UpdateService.java
@@ -13,15 +13,34 @@ import java.util.List;
 
 @Service
 public class UpdateService {
+    StringBuilder sb = new StringBuilder();
     public void updateReadme() throws IOException {
-        StringBuilder sb = new StringBuilder();
+
+
         sb.append("# tech-blog-commit-bot\n").append("유명 기업의 기술 블로그의 글을 매일 매일 갱신해주는 봇 \uD83E\uDD16\n");
         LocalDateTime date = LocalDateTime.now().plusHours(9);
 
         sb.append("## ⏲ ").append(date.getYear()).append("년 ").append(date.getMonthValue()).append("월 ").append(date.getDayOfMonth()); //.append("일의 업데이트 내용입니다.").append("\n");
         sb.append("일 ").append(date.getHour()).append("시 ").append(date.getMinute()).append("분의 업데이트 내용입니다.\n");
-        String[] docs = {"https://prgms.tistory.com/"};
-        Document doc = Jsoup.connect(docs[0]).get();
+
+        programmers();
+
+        File readme = new File("README.md");
+        // 파일이 없다면 새로 만들고, 파일이 있다면 덮어씌운다.
+        FileWriter writer = new FileWriter(readme, false);
+        writer.write(sb.toString());
+        writer.close();
+    }
+
+    private String titleToLink(String title, String link) {
+        return "[" + title + "](" + link + ")" + "\n\n";
+    }
+
+
+    private void programmers() throws IOException {
+
+        String programmersUrl = "https://prgms.tistory.com/";
+        Document doc = Jsoup.connect(programmersUrl).get();
         List<Element> last = doc.select("#main > div > div.index_type_common.index_type_post.index_type_post_line4 > div.article_content > a.link_title");
         List<Element> elements = doc.select("#main > div > div.index_type_common.index_type_gallery > ul > li > div > a.link_title");
 
@@ -36,17 +55,5 @@ public class UpdateService {
             String link = "https://prgms.tistory.com" + element.attr("href");
             sb.append(titleToLink(title, link));
         }
-
-        File readme = new File("README.md");
-        // 파일이 없다면 새로 만들고, 파일이 있다면 덮어씌운다.
-        FileWriter writer = new FileWriter(readme, false);
-        writer.write(sb.toString());
-        writer.close();
     }
-
-    private String titleToLink(String title, String link) {
-        return "[" + title + "](" + link + ")" + "\n\n";
-    }
-
-
 }
