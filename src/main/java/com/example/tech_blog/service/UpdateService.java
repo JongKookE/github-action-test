8867eb0d31a627c615e9e9809c65143196cd6955
박종국 <whdrnrdl789@naver.com>
2024-07-28 14:03:31 +0900

    feat: add new headers

--- src/main/java/com/example/tech_blog/service/UpdateService.java
+++ src/main/java/com/example/tech_blog/service/UpdateService.java
@@ -62,6 +62,13 @@ public class UpdateService {
         String woowahanUrl = "https://techblog.woowahan.com";
         Document doc = Jsoup.connect(woowahanUrl)
                 .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36")
+                .header("scheme", "https")
+                .header("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8")
+                .header("accept-encoding", "gzip, deflate, br")
+                .header("accept-language", "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,es;q=0.6")
+                .header("cache-control", "no-cache")
+                .header("pragma", "no-cache")
+                .header("upgrade-insecure-requests", "1")
                 .get();
         List<Element> titles = doc.select("div.post-list div.post-item a");
         int indexSize = 10;
