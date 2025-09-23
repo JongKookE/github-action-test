bae48e3ead7cc8fe93654ddbed244296fd49867f
박종국 <whdrnrdl789@naver.com>
2025-09-24 00:27:47 +0900

    우아한 형제들 포스트 추가

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # tech-blog-commit-bot
 기술 블로그의 글을 매일 매일 갱신해주는 봇 🤖
-## ⏲ 2025년 9월 23일 13시 26분의 업데이트 내용입니다.
+## ⏲ 2025년 9월 24일 9시 25분의 업데이트 내용입니다.
 ## 🎃프로그래머스
 
 ---
@@ -13,4 +13,27 @@
 - ['2022 Dev-Matching: 웹 프론트엔드 개발자(상반기)' 과제 테스트 해설](https://prgms.tistory.com//139)
 
 - [개발자를 위해 준비했어요! 머쓱이의 '머쓱한 ZOOM 배경화면'](https://prgms.tistory.com//100)
+
+## 🎃우아한 형제들
+
+---
+- [우아한테크코스 8기: AI가 코드 짜는 시대, 그래도 개발자가 되시겠습니까?](https://techblog.woowahan.com/22828/)
+
+- [Spring Cache(@Cacheable) + Spring Data Redis 사용 시 record 직렬화 오류 원인과 해결](https://techblog.woowahan.com/22767/)
+
+- [Java야…, 우리 그만 헤어져. Kotlin으로 환승연애](https://techblog.woowahan.com/22586/)
+
+- [배차 정확도를 높이는 실거리 시스템 구축하기: OSRM, Kafka, 그리고 Redis](https://techblog.woowahan.com/22396/)
+
+- [우리 서비스와 연결된 MCP Server 빠르게 구현해보기: MCP 해커톤 후기](https://techblog.woowahan.com/22342/)
+
+- [B마트 OMS의 물류주문관리를 통한 출고 최적화](https://techblog.woowahan.com/22263/)
+
+- [JavaScript Proxy를 활용한 상태 추적 도구 개발기](https://techblog.woowahan.com/22246/)
+
+- [우아한형제들 PM들이 추천하는 책(ver.2025)](https://techblog.woowahan.com/22211/)
+
+- [전체 서비스를 관통하는 도메인 모듈을 안전하게 분리하기](https://techblog.woowahan.com/22151/)
+
+- [실시간 마케팅을 위한 PoC 개발기](https://techblog.woowahan.com/22043/)
 
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,6 +16,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         readmeFormatter.actionStart(sb);
         programmers();
+        wooahan();
         File readme = new File("README.md");
         // 파일이 없다면 새로 만들고, 파일이 있다면 덮어씌운다.
         FileWriter writer = new FileWriter(readme, false);
@@ -39,6 +40,20 @@ public class Main {
         for (Element element: elements) {
             String title = element.text();
             String link = TechBlogUrl.PROGRAMMERS.getUrl() + element.attr("href");
+            readmeFormatter.titleToLink(sb, title, link);
+        }
+    }
+
+    public static void wooahan() throws IOException {
+        readmeFormatter.setH2(sb, TechBlogName.WOOAHAN.getName());
+        Document doc = Jsoup.connect(TechBlogUrl.WOOAHAN.getUrl()).get();
+        List<Element> titles = doc.select("div.post-list div.post-item a");
+        int indexSize = 10;
+        // indexSize를 조정안하고 for-each를 사용하면 알수없는 태그들도 같이 넘어와서 포스트의 사이즈로 순회
+        for(int index = 0; index < indexSize; index++){
+            Element element = titles.get(index);
+            String title = element.select("h2.post-title").text();
+            String link = element.select("a").attr("href");
             readmeFormatter.titleToLink(sb, title, link);
         }
     }
